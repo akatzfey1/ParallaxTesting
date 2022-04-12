5f1f89d0a8731a6fff208f81c2d8fc8132ff0845
Alex Katzfey <akatzfey@yahoo.com>
2022-04-12 14:03:46 -0700

    Initial Commit

---
+++ Parallax/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Parallax
+//
+//  Created by Alex Katzfey on 4/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Parallax/ParallaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxApp.swift
+//  Parallax
+//
+//  Created by Alex Katzfey on 4/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
